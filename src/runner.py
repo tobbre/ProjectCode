0613e9ffb36da8f0262f77391b48f926decba3a2
tobbre <tobias.breuer2@gmail.com>
2023-03-16 15:40:36 +0100

    commit

--- src/runner.py
+++ src/runner.py
@@ -32,50 +32,62 @@ def read_compute_print_kcores(fi="1m", k=9):
 
 
 def minnodecut_draw(G):
+	a = time.time()
 	H = copy.deepcopy(G)
+	b = time.time()
+	print("Copying G: ", str(b - a))
 	cutset = nx.minimum_node_cut(H)
+	c = time.time()
+	print("Fidning mininmum node cut: ", str(c - b))
 	pos = nx.spring_layout(H, seed=3113794652)
 	H.remove_nodes_from(cutset)
 	comps, largest = algorithms.con_comps_and_largest(H)
-	options = {"node_size": 5}
-	nx.draw_networkx_nodes(H, pos, nodelist=comps[0], node_color="tab:blue", **options)
-	nx.draw_networkx_nodes(H, pos, nodelist=comps[1], node_color="tab:green", **options)
-	for i in range(2, 4):
-		if len(comps) > i:
+	d = time.time()
+	print("Find connected components: ", str(d - c))
+	options = {"node_size": 4}
+	nx.draw_networkx_nodes(H, pos, nodelist=comps[largest], node_color="tab:blue", **options)
+	for i in range(4):
+		if len(comps) > i and i != largest:
 			nx.draw_networkx_nodes(H, pos, nodelist=comps[i], node_color="tab:green", **options)
 	nx.draw_networkx_nodes(H, pos, nodelist=cutset, node_color="tab:red", **options)
 	# edges
-	nx.draw_networkx_edges(H, pos, width=0.2)
+	nx.draw_networkx_edges(H, pos, width=0.1)
+	plt.savefig("out/10m11cut%s" % timestamp)
+	e = time.time()
+	print("drawing figure: ", str(e - d))
+
+	plt.clf()
 	with open(file="out/comps.txt", mode="a") as f:
 		print("\n"*2)
-		print("largest component is index " + str(largest) + ":")
+		print("largest component at %s is index " % timestamp + str(largest) + ". All components:")
 		for i in range(len(comps)):
 			print(comps[i])
 
 	return comps, largest
 
 
+# reads graph files and draws them
+# cores = nx.read_adjlist("out/kcores10m9_13-03-2023_18:40")
+# nx.draw(cores, node_size=4, width=0.1)
+# plt.savefig("out/kcores10m9_13-03-2023_18:40")
+# plt.clf()
+# for k in range(10, 16):
+# 	a = time.time()
+# 	cores = nx.read_adjlist("out/kcores10m%s_15-03-2023_19:09" % k)
+# 	b = time.time()
+# 	print("Read graph: " + str(b - a))
+# 	nx.draw(cores, node_size=4, width=0.1)
+# 	plt.savefig("out/kcores10m%s_15-03-2023_19:09" % k)
+# 	c = time.time()
+# 	print("Draw & Save figure: " + str(c - b))
+# 	plt.clf()
 
-cores = nx.read_adjlist("out/kcores10m9_13-03-2023_18:40")
-nx.draw(cores, node_size=4, width=0.1)
-plt.savefig("out/kcores10m9_13-03-2023_18:40")
-plt.clf()
-for k in range(10, 16):
-	a = time.time()
-	cores = nx.read_adjlist("out/kcores10m%s_15-03-2023_19:09" % k)
-	b = time.time()
-	print("Read graph: " + str(b - a))
-	nx.draw(cores, node_size=4, width=0.1)
-	plt.savefig("out/kcores10m%s_15-03-2023_19:09" % k)
-	c = time.time()
-	print("Draw & Save figure: " + str(c - b))
-	plt.clf()
 
-
-# G = nx.read_adjlist("out/H")
-# comps, largest = minnodecut_draw(G)
-# H = algorithms.remove_smaller_connected_components(G, comps, largest)
-# nx.write_adjlist(H, "out/H%s" % timestamp)
+# reads graph files and finds components, removes all but biggest
+G = nx.read_adjlist("out/kcores10m11_15-03-2023_19:09")
+comps, largest = minnodecut_draw(G)
+H = algorithms.remove_smaller_connected_components(G, comps, largest)
+nx.write_adjlist(H, "out/H10m11%s" % timestamp)
 
 
 
